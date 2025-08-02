ca0f21aa9241801090c9062053b07e17b3859e94
Joyjeet Sarkar <joy@amoga.io>
2025-08-02 20:02:08 +0530

    .

--- obsidian/_meta.js
+++
@@ -1,9 +0,0 @@
-// obsidian/_meta.js - Navigation configuration for Obsidian vault
-export default {
-  index: "Welcome",
-  "getting-started": "Getting Started",
-  guides: {
-    title: "Guides",
-    type: "page",
-  },
-};
--- obsidian/guides/_meta.js
+++
@@ -1,4 +0,0 @@
-// obsidian/guides/_meta.js - Navigation for guides section
-export default {
-  "advanced-integration": "Advanced Integration",
-};
